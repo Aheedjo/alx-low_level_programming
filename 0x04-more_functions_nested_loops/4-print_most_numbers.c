563e0ba7a541b636b5e71203ef474fa440480e0a
Aheedjo <maishanu.ahijo@yahoo.com>
2023-09-14 17:39:54 -0700

    update

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,16 +6,16 @@
  */
 void print_most_numbers(void)
 {
-	char num = '0';
+    char num = '0';
 
-	while (num <= '9')
-	{
-		if (num != '2' && num != '4')
-		{
-			_putchar(num);
-		}
-		num++;
-	}
+    while (num <= '9')
+    {
+        if (num != '2' && num != '4')
+        {
+            _putchar(num);
+        }
+        num++;
+    }
 
-	_putchar('\n');
+    _putchar('\n');
 }
